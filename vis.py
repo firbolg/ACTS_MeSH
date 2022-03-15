c8f1bebcbf69b705b94441f6a176f2d3cae49360
Dolan <dolanl@iu.edu>
2022-03-15 16:42:05 -0400

    keyword counter

--- vis.py
+++ vis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import csv
 
 def getmesh(citations):
     mesh = 'MH  -'
@@ -19,6 +20,25 @@ def getmesh(citations):
               pass
     return PIs
 
+def getkeywords(citations):
+    keywords = 'OT  -'
+    mod = [i.split('\n') for i in citations]
+    combined = [i for sublist in mod for i in sublist]
+    keywords = [i for i in combined if i.startswith('OT  - ')]
+    keywords = [i.split('/') for i in keywords]
+    kw = []
+    for i in keywords:
+        for w in i:
+            if w.startswith('OT  - *'):
+              w = (w[7:])
+              kw.append(w)
+            if w.startswith('OT  - '):
+              w = (w[6:])
+              kw.append(w)
+            else:
+              pass
+    return kw
+
 
 #Create a dictionary from previous indexing MeSH terms with term as key, total count of term occurrences as value
 def PI_dict(terms):
@@ -36,20 +56,19 @@ def PI_data(term_list):
     PIs = dict(sorted(PIs.items(), reverse=True, key=lambda t: t[::-1]))
     total = sum(PIs.values())
 
-    for key, value in PIs.items():
-        pct = value * 100 / total
-        PIs[key] = round(pct, 2)
-
     PI_k = sorted(PIs, key=lambda x: (-PIs[x], x))
     PI_v = list(PIs.values())
 
-    PI_sorted_dict = {
-                      'PI 1':PI_k[0], 'PI 1 %':PI_v[0], 'PI 2':PI_k[1], 'PI 2 %':PI_v[1], 'PI 3':PI_k[2], 'PI 3 %':PI_v[2],
-                      'PI 4':PI_k[3],'PI 4 %':PI_v[3], 'PI 5':PI_k[4], 'PI 5 %':PI_v[4], 'PI 6':PI_k[5], 'PI 6 %':PI_v[5], 
-                      'PI 7':PI_k[6], 'PI 7 %': PI_v[6], 'PI 8':PI_k[7], 'PI 8 %':PI_v[7], 'PI 9':PI_k[8], 'PI 9 %':PI_v[8], 
-                      'PI 10':PI_k[9], 'PI 10 %':PI_v[9], 
-                      'Total PIs':total}
-    return PI_sorted_dict 
+    new_dict = {'MeSH Term':PI_k, 'Total':PI_v}
+
+    #PI_sorted_dict = {
+                    #  'PI 1':PI_k[0], 'PI 1 %':PI_v[0], 'PI 2':PI_k[1], 'PI 2 %':PI_v[1], 'PI 3':PI_k[2], 'PI 3 %':PI_v[2],
+                     # 'PI 4':PI_k[3],'PI 4 %':PI_v[3], 'PI 5':PI_k[4], 'PI 5 %':PI_v[4], 'PI 6':PI_k[5], 'PI 6 %':PI_v[5], 
+                    #  'PI 7':PI_k[6], 'PI 7 %': PI_v[6], 'PI 8':PI_k[7], 'PI 8 %':PI_v[7], 'PI 9':PI_k[8], 'PI 9 %':PI_v[8], 
+                     # 'PI 10':PI_k[9], 'PI 10 %':PI_v[9], 
+                     # 'Total PIs':total}
+
+    return new_dict
 
 
 df_source = pd.read_csv('ACTS_CTSI_Health_Equity_Articles_20220209.csv')
@@ -57,5 +76,12 @@ df_source = pd.read_csv('ACTS_CTSI_Health_Equity_Articles_20220209.csv')
 f = open('pubmed.txt', 'r')
 
 top_mesh = PI_data(getmesh(f))
+top_keywords = PI_data(getkeywords(f))
 
-print(top_mesh)
+df = pd.DataFrame(top_mesh)
+df1 = pd.DataFrame(top_keywords)
+
+df.to_csv('MeSH Totals.csv')
+df1.to_csv('Keyword Totals.csv')
+
+
